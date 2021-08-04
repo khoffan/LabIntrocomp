075e889271185785dd5845d0255e3ed8d75ae51f
khoffan <6210110458@psu.ac.th>
2021-08-04 23:31:35 +0700

    ubdate lab3.6

---
+++ lab3-2-2/lab3-2-6.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int i,sum=0;
+    char c;
+    for(i=0;i<=50;i++){
+        printf("Box: ",i);
+        scanf("\n%c",&c);
+        if( c == '+'){
+            sum += 1;
+        }
+        else if( c == '-'){
+            sum -= 1;
+        }
+        else if( c == 'q'){
+            printf("sum = %d",sum);
+            break;
+        }
+        if(sum == 5){
+            printf("FUUL");
+            break;
+        }
+    }
+    return 0;
+}
